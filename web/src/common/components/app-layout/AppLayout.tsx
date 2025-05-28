380a170e23258c02a6114eb42be2dfdcc56a23f7
gsousa12 <wgsousa12@gmail.com>
2025-05-28 20:13:20 -0300

    front

--- web/src/common/components/app-layout/AppLayout.tsx
+++ web/src/common/components/app-layout/AppLayout.tsx
@@ -5,7 +5,7 @@ import { GlobalWrapper } from "../wrappers/global-wrapper/GlobalWrapper";
 import { GlobalLoader } from "../loader/GlobalLoader";
 import { Header } from "../header/Header";
 
-export const mockIsAuthenticated = false;
+export const mockIsAuthenticated = true;
 
 export const AppLayout = () => {
   const isAuthenticated = mockIsAuthenticated;
